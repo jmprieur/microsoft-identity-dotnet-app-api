a92fec38660a8ecca00f50feaed9931af6db1ed4
Jean-Marc Prieur <jmprieur@microsoft.com>
2019-10-30 15:29:10 +0100

    Adding displaying the owner of the app

--- FindAppsWithExpiredCerts/FindAppsWithExpiredCerts/GraphModel.cs
+++ FindAppsWithExpiredCerts/FindAppsWithExpiredCerts/GraphModel.cs
@@ -12,6 +12,7 @@ namespace FindAppsWithExpiredCerts
     public class App
     {
         public string displayName { get; set; }
+        public string id { get; set; }
         public string appId { get; set; }
         public KeyCredential[] keyCredentials { get;set;}
     }
@@ -19,5 +20,15 @@ namespace FindAppsWithExpiredCerts
     public class Apps
     {
         public App[] value { get; set; }
+    }
+
+    public class Users
+    {
+        public User[] value { get; set; }
+    }
+
+    public class User
+    {
+        public string userPrincipalName { get; set; }
     }
 }
--- FindAppsWithExpiredCerts/FindAppsWithExpiredCerts/Program.cs
+++ FindAppsWithExpiredCerts/FindAppsWithExpiredCerts/Program.cs
@@ -9,14 +9,16 @@ namespace FindAppsWithExpiredCerts
 {
     class Program
     {
+        // App configuration
+        static private string tenant = "";
+        static private string clientId = "";
+
+        // Graph app API
+        static private string[] scopes = new[] { "Directory.AccessAsUser.All", "User.ReadBasic.All" };
+        static private string endpoint = "https://graph.microsoft.com/beta/applications";
+
         static async Task Main(string[] args)
         {
-            // App configuration
-            string tenant = "";
-            string clientId = "";
-            string[] scopes = new[] { "Directory.AccessAsUser.All" };
-            string endpoint = "https://graph.microsoft.com/beta/applications";
-
             // Create the app.
             var app = PublicClientApplicationBuilder.Create(clientId)
                 .WithTenantId(tenant)
@@ -30,20 +32,23 @@ namespace FindAppsWithExpiredCerts
             // Call the protected API
             HttpClient httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Add("Authorization", result.CreateAuthorizationHeader());
-            string jsonString = await httpClient.GetStringAsync(endpoint);
+            string appsJsonString = await httpClient.GetStringAsync(endpoint);
 
             // Get apps from Graph
-            Apps apps = JsonSerializer.Deserialize<Apps>(jsonString);
+            Apps apps = JsonSerializer.Deserialize<Apps>(appsJsonString);
 
             // Find expired certificates
-            var appsWithExpiredCerts = apps.value.Select(a => new { app = a, expiredCerts = a.keyCredentials.Where(k => k.endDateTime < DateTime.Now) })
+            var appsWithExpiredCerts = apps.value.Select(a => new { app = a, 
+                                                                    owners=GetOwners(httpClient, a).Result,
+                                                                    expiredCerts = a.keyCredentials
+                                                                                    .Where(k => k.endDateTime < DateTime.Now) })
                                                  .Where(a => a.expiredCerts.Any());
 
             // Display the apps with their expired certificates
-            foreach(var appWithExpiredCerts in appsWithExpiredCerts)
+            foreach (var appWithExpiredCerts in appsWithExpiredCerts)
             {
                 var a = appWithExpiredCerts.app;
-                Console.WriteLine($"App displayName={a.displayName} AppId={a.appId} ");
+                Console.WriteLine($"App displayName={a.displayName} AppId={a.appId} Owners='{appWithExpiredCerts.owners}' ");
                 foreach(var expiredCert in appWithExpiredCerts.expiredCerts)
                 {
                     Console.WriteLine($"- EXPIRED Certificate '{expiredCert.displayName}' Id={expiredCert.customKeyIdentifier}");
@@ -51,6 +56,16 @@ namespace FindAppsWithExpiredCerts
             }
 
 
+        }
+
+
+        private static async Task<string> GetOwners(HttpClient httpClient, App app)
+        {
+            // Find their owners
+            string ownerUri = $"{endpoint}/{app.id}/owners";
+            string ownersJsonString = await httpClient.GetStringAsync(ownerUri);
+            Users users = JsonSerializer.Deserialize<Users>(ownersJsonString);
+            return string.Join(",", users.value.Select(u => u.userPrincipalName));
         }
     }
 }
